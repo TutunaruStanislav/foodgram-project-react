17571a3a4a08c596321b01eeff7775b0308ad4a0
Stanislav Tutunaru <tutunaru.stanislav@yandex.ru>
2022-08-26 18:16:09 +0500

    change filters

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -10,8 +10,8 @@ class IngredientFilter(SearchFilter):
 
 class RecipeFilter(FilterSet):
     tags = filters.AllValuesMultipleFilter(field_name='tags__slug')
-    is_favorited = filters.BooleanFilter(method='filter_is_favorited')
-    is_in_shopping_cart = filters.BooleanFilter(
+    is_favorited = filters.NumberFilter(method='filter_is_favorited')
+    is_in_shopping_cart = filters.NumberFilter(
         method='filter_is_in_shopping_cart'
     )
 
--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -10,11 +10,3 @@ class IsAuthorOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         return (request.method in permissions.SAFE_METHODS
                 or obj.author == request.user)
-
-
-class IsAdminOrReadOnly(permissions.BasePermission):
-
-    def has_permission(self, request, view):
-        return (request.user.is_authenticated
-                and request.user.is_admin
-                or request.method in permissions.SAFE_METHODS)
